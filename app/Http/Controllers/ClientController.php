7f5bb7bb80330290e23e20a3ac7601ed4eae4ee8
Celeste Caterino <celestecaterino@gmail.com>
2021-05-17 23:23:39 +0200

    Created listing view with pagination

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,7 +14,8 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        $clients = Client::paginate(10);
+        return view('clients.index')->with('clients', $clients);
     }
 
     /**
@@ -45,40 +46,6 @@ class ClientController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layout')
+
+@section('content')
+<div class="container mt-5">
+    <h1>Clients</h1>
+    <div class="d-flex justify-content-end mb-2">
+        <a href="{{route('clients.create')}}" class="btn btn-primary">Add new</a>
+    </div>
+    <div class="table-responsive-sm">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">First name</th>
+                    <th scope="col">Last name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone number</th>
+                    <th scope="col">Gender</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($clients as $client)
+                <tr>
+                    <td>{{$client->first_name}}</td>
+                    <td>{{$client->last_name}}</td>
+                    <td>{{$client->email}}</td>
+                    <td>{{$client->phone_number}}</td>
+                    <td>{{$client->gender}}</td>
+                    <td>
+                        <a href="{{route('clients.show', ['client' => $client->id])}}">Detail</a>
+                    </td>
+                </tr>
+                @empty
+                No records
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex justify-content-end">
+        {{$clients->links()}}
+    </div>
+</div>
+@endsection
+
