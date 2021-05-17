cf21bddd4c59da777859a5b974276f12e5e8df39
Celeste Caterino <celestecaterino@gmail.com>
2021-05-17 20:59:31 +0200

    Created ClientController and updated routes ⏩

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('clients.index');
 });
+
+Route::resource('clients', ClientController::class)->except([
+    'edit', 'update', 'destroy'
+]);
